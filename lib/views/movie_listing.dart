8931efe65567ff3bb16bf98906cc4083450d0ccc
ndurve06 <neerav.durve@gmail.com>
2026-09-24 11:50:47 +0100

    Arrange movie listing layout with Column and Row

--- lib/views/movie_listing.dart
+++ lib/views/movie_listing.dart
@@ -18,16 +18,47 @@ class MovieListing extends StatelessWidget {
         // body: const SizedBox.shrink(),
         body: Container(
             color: cinemaBackground,
-            child: Row(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   "Ted (2012) (15)",
                   style: TextStyle(
-                    fontSize: 25,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
                 Text(
                     "A wish comes true for John when his teddy bear, Ted, comes to life."),
+                Row(
+                  children: const [
+                    Text("Southsea Cinema Room"),
+                  ],
+                ),
+                Row(
+                  children: const [
+                    Text("Friday 25 December 2026, 18:00 - ends at 19:14")
+                  ],
+                ),
+                Row(
+                  children: const [
+                    Text("Please note that Discounts / Membership Benefits "),
+                    Text("will be applied once you have selected your tickets")
+                  ],
+                ),
+                Row(
+                  children: const [Text("Select Quantities (Upto 5 in total)")],
+                ),
+                Row(
+                  children: const [Text("Tickets")],
+                ),
+                Row(
+                  children: const [Text("Adult (£7.50)")],
+                ),
+                Row(
+                  children: const [Text("ADD TO ORDER")],
+                ),
               ],
             )));
   }
