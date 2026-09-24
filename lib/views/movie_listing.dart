f4aa6e1e79772b41a113d2cf2eda88970bb4977f
ndurve06 <neerav.durve@gmail.com>
2026-09-24 10:51:52 +0100

    Add film title and description container

--- lib/views/movie_listing.dart
+++ lib/views/movie_listing.dart
@@ -8,14 +8,22 @@ class MovieListing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(appTitle, style: cinemaHeaderStyle),
-        backgroundColor: cinemaSurface,
-        iconTheme: const IconThemeData(color: cinemaBrand),
-        elevation: 0,
-      ),
-      drawer: const NavDrawer(),
-      body: const SizedBox.shrink(),
-    );
+        appBar: AppBar(
+          title: const Text(appTitle, style: cinemaHeaderStyle),
+          backgroundColor: cinemaSurface,
+          iconTheme: const IconThemeData(color: cinemaBrand),
+          elevation: 0,
+        ),
+        drawer: const NavDrawer(),
+        // body: const SizedBox.shrink(),
+        body: Container(
+            color: cinemaBackground,
+            child: Row(
+              children: [
+                Text("Ted (2012) (15)"),
+                Text(
+                    "A wish comes true for John when his teddy bear, Ted, comes to life."),
+              ],
+            )));
   }
 }
